61bd78c8d9841d6219c7f66c06624cb3adc4d15e
masa <masa.kt92@gmail.com>
2018-09-13 13:57:07 +0200

    projet

--- code/LoncotoFront/LoncotoAppB/src/app/metier/relclientsite.ts
+++
@@ -1,9 +0,0 @@
-export class RelClientSite{
-
-    public constructor(public clientId : number,public siteId:number){}
-
-    public copyFrom(source : RelClientSite){
-        this.clientId = source.clientId;
-        this.siteId =this.siteId;
-    }
-}
